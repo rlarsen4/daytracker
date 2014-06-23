b41f0367e57a679a89a3875d96f84c3100605b55
Rick Larsen <ricklarsen4@yahoo.com>
2014-06-23 11:58:38 -0700

    beginning dev of dashboard.

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 daytracker
 ==========
 
+Description
+===========
+daytracker is a web app to monitor student progress in immersion classes. When entered into student database, daytracker will alert the teacher when milestones are met and will generate report cards and other reports as needed. 
+
+
+
+
 Great repository names are short and memorable. Need inspiration? How about bearded-octo-hipster.
---
+++ dashboard.php
@@ -0,0 +1,15 @@
+<div class="main"> 
+
+    <!-- alerts display here -->
+    <div class="alerts">  Alerts
+        <?php  
+            echo "Pants, Smarty needs to be tested."
+            //echo alerts here
+        ?>
+    </div>
+    <div class="controls">
+        <button class="editCalendar">Edit no school days</button>
+        <button class="addUpdateStudent">Add/Update</button>
+        <button class="reports">Reports</button>
+    </div>
+</div>
